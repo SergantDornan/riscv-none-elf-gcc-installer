2ea5b05fab6280e15a33b3f7f29c76b0cf194c3a
SergantDornan <microsoftvis@gmail.com>
2025-03-04 22:02:09 +0300

    multiple terminal configs support

--- include/exportPath.h
+++ include/exportPath.h
@@ -4,7 +4,7 @@
 #include "BuilderFilework.h"
 const std::vector<std::string> configFiles = {getHomedir() + "/.bashrc", 
 	getHomedir() + "/.zshrc", getHomedir() + "/.config/fish/config.fish"};
-std::string findConfig();
+std::vector<std::string> findConfig();
 std::string getLine(const std::string&, const std::string&);
 void addPath(const std::string&);
 void removePath(const std::string&);
--- source/exportPath.cpp
+++ source/exportPath.cpp
@@ -1,10 +1,11 @@
 #include "exportPath.h"
-std::string findConfig(){
+std::vector<std::string> findConfig(){
+	std::vector<std::string> v;
 	for(int i = 0; i < configFiles.size(); ++i){
 		if(exists(configFiles[i]))
-			return configFiles[i];
+			v.push_back(configFiles[i]);
 	}
-	return "-1";
+	return v;
 }
 std::string getLine(const std::string& config, const std::string& path){
 	std::string name = getName(config);
@@ -15,51 +16,55 @@ std::string getLine(const std::string& config, const std::string& path){
 	return "-1";
 }
 void addPath(const std::string& path){
-	std::string config = findConfig();
-	if(config == "-1"){
+	std::vector<std::string> config = findConfig();
+	if(config.size() == 0){
 		std::cout << "======================= ERROR =======================" << std::endl;
 		std::cout << "exportPath.cpp: addPath: cannot find config file " << std::endl;
 		std::cout << "probably does not work for your system" << std::endl;
 		std::cout << "cannot add exportPath" << std::endl;
 		return;
 	}
-	std::string line = getLine(config, path);
-	if(line == "-1"){
-		std::cout << "======================= ERROR =======================" << std::endl;
-		std::cout << "exportPath.cpp: addPath: some error in: " << std::endl;
-		std::cout << "exportPath.cpp: getLine" << std::endl;
-		return;
+	for(int i = 0; i < config.size(); ++i){
+		std::string line = getLine(config[i], path);
+		if(line == "-1"){
+			std::cout << "======================= ERROR =======================" << std::endl;
+			std::cout << "exportPath.cpp: addPath: some error in: " << std::endl;
+			std::cout << "exportPath.cpp: getLine" << std::endl;
+			return;
+		}
+		std::ofstream bshrc(config[i], std::ios::app);
+    	bshrc << line << std::endl;
+    	bshrc.close();
 	}
-	std::ofstream bshrc(config, std::ios::app);
-    bshrc << line << std::endl;
-    bshrc.close();
 }
 void removePath(const std::string& path){
-	std::string config = findConfig();
-	if(config == "-1"){
+	std::vector<std::string> config = findConfig();
+	if(config.size() == 0){
 		std::cout << "======================= ERROR =======================" << std::endl;
 		std::cout << "exportPath.cpp: removePath: cannot find config file " << std::endl;
 		std::cout << "probably does not work for your system" << std::endl;
 		std::cout << "cannot remove exportPath" << std::endl;
 		return;
 	}
-	std::string line = getLine(config, path);
-	if(line == "-1"){
-		std::cout << "======================= ERROR =======================" << std::endl;
-		std::cout << "exportPath.cpp: removePath: some error in: " << std::endl;
-		std::cout << "exportPath.cpp: getLine" << std::endl;
-		return;
+	for(int i = 0; i < config.size(); ++i){
+		std::string line = getLine(config[i], path);
+		if(line == "-1"){
+			std::cout << "======================= ERROR =======================" << std::endl;
+			std::cout << "exportPath.cpp: removePath: some error in: " << std::endl;
+			std::cout << "exportPath.cpp: getLine" << std::endl;
+			return;
+		}
+    	std::string l;
+    	std::vector<std::string> v;
+    	std::ifstream file(config[i]);
+    	while(std::getline(file,l)){
+        	if(l != line)
+            	v.push_back(line);
+    	}
+    	file.close();
+    	std::ofstream newfile(config[i]);
+    	for(int i = 0; i < v.size(); ++i)
+        	newfile << v[i] << std::endl;
+    	newfile.close();
 	}
-    std::string l;
-    std::vector<std::string> v;
-    std::ifstream file(config);
-    while(std::getline(file,l)){
-        if(l != line)
-            v.push_back(line);
-    }
-    file.close();
-    std::ofstream newfile(config);
-    for(int i = 0; i < v.size(); ++i)
-        newfile << v[i] << std::endl;
-    newfile.close();
 }
